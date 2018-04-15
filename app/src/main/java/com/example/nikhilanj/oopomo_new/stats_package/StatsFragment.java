a6050d358ddf3d2c954b25fe5f9a908ac9c8e3ad
P V Nikhilanj <nikhilanj97@gmail.com>
2018-04-15 15:21:42 +0530

    Basic stats graphs in place

--- app/src/main/java/com/example/nikhilanj/oopomo_new/stats_package/StatsFragment.java
+++ app/src/main/java/com/example/nikhilanj/oopomo_new/stats_package/StatsFragment.java
@@ -1,9 +1,11 @@
 package com.example.nikhilanj.oopomo_new.stats_package;
 
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -48,10 +50,21 @@ class DayValueFormatter implements IAxisValueFormatter {
         return (new SimpleDateFormat("dd",Locale.getDefault()).format(timeInMillis)+"-"+
                 new SimpleDateFormat("MMM",Locale.getDefault()).format(timeInMillis));
     }
+    static String getMonthStringFromTimeMillis(long timeInDays){
+        long timeInMillis = TimeUnit.DAYS.toMillis(timeInDays);
+        return (new SimpleDateFormat("MMMM",Locale.getDefault()).format(timeInMillis));
+    }
+
 
     @Override
     public String getFormattedValue(float value, AxisBase axis){
-        String x = getDateMonthStringFromTimeMillis((long)value);
+        String x = "0";
+        if(StatsFragment.currentViewRange==StatsFragment.VIEWRANGE_DAILY) {
+            x = getDateMonthStringFromTimeMillis((long) value);
+        }
+        else if(StatsFragment.currentViewRange==StatsFragment.VIEWRANGE_MONTHLY) {
+            x = getMonthStringFromTimeMillis((long) value);
+        }
         return x;
     }
 }
@@ -61,8 +74,17 @@ public class StatsFragment extends Fragment{
     public StatsFragment() {}
 
     private TextView statstextview;
+    private BarChart statsBarChart;
 
-    private List<StatObject> statsList = new ArrayList<>();
+    private List<StatObject> statsListDaily = new ArrayList<>();
+    private List<StatObject> statsListWeekly = new ArrayList<>();
+    private List<StatObject> statsListMonthly = new ArrayList<>();
+
+    final public static int VIEWRANGE_DAILY   = 0;
+    final public static int VIEWRANGE_WEEKLY  = 1;
+    final public static int VIEWRANGE_MONTHLY = 2;
+
+    public static int currentViewRange = VIEWRANGE_DAILY;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -74,26 +96,21 @@ public class StatsFragment extends Fragment{
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.stats_fragment_layout, container, false);
 
-        statsList.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("08-4-2018"),2,0));
-        statsList.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("09-4-2018"),25,17));
-        statsList.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("10-4-2018"),15,10));
-        statsList.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("11-4-2018"),20,15));
-        statsList.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("12-4-2018"),45,20));
-        statsList.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("13-4-2018"),35,10));
-        statsList.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("14-4-2018"),25,5));
+
 
         TabLayout statsTabLayout = view.findViewById(R.id.stats_tab_layout);
 
         statstextview = view.findViewById(R.id.statstext);
         statstextview.setVisibility(View.INVISIBLE);
 
-        final BarChart statsBarChart = view.findViewById(R.id.stats_bar_chart);
+        statsBarChart = view.findViewById(R.id.stats_bar_chart);
         statsBarChart.getDescription().setEnabled(false);
         //statsBarChart.setPinchZoom(false);
 
         statsBarChart.setDrawGridBackground(false);
         statsBarChart.setDrawBarShadow(false);
         statsBarChart.setGridBackgroundColor(Color.WHITE);
+        statsBarChart.setClickable(false);
 
         statsBarChart.setDrawValueAboveBar(false);
         statsBarChart.setHighlightFullBarEnabled(false);
@@ -111,44 +128,11 @@ public class StatsFragment extends Fragment{
         xLabels.setValueFormatter(new DayValueFormatter());
         xLabels.setGranularity(1f);
         xLabels.setDrawGridLines(true);
-        xLabels.setLabelCount(7,true);
+        xLabels.setLabelCount(8,true);
 
         statsBarChart.getLegend().setEnabled(false);
 
-        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
-
-        for (int i = 0; i < statsList.size(); i++) {
-            StatObject stat = statsList.get(i);
-            float focustime = (float) stat.getFocusTime();
-            float breaktime = (float) stat.getBreakTime();
-            System.out.println(yVals1);
-
-            yVals1.add(
-                    new BarEntry(TimeUnit.MILLISECONDS.toDays(stat.getTimeStamp()),new float[]{focustime, breaktime}));
-        }
-
-        BarDataSet set1;
-
-        if (statsBarChart.getData() != null &&
-                statsBarChart.getData().getDataSetCount() > 0) {
-            set1 = (BarDataSet) statsBarChart.getData().getDataSetByIndex(0);
-            set1.setValues(yVals1);
-            statsBarChart.getData().notifyDataChanged();
-            statsBarChart.notifyDataSetChanged();
-        } else {
-            set1 = new BarDataSet(yVals1, "Your Daily Stats");
-            set1.setDrawIcons(false);
-            set1.setColors(getColors());
-            set1.setStackLabels(new String[]{"Focus Time", "Break Time"});
-
-            List<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
-            dataSets.add(set1);
-
-            BarData data = new BarData(dataSets);
-            data.setValueTextColor(Color.WHITE);
-
-            statsBarChart.setData(data);
-        }
+        showChart(VIEWRANGE_DAILY);
 
         statsBarChart.setFitBars(true);
         statsBarChart.invalidate();
@@ -157,15 +141,18 @@ public class StatsFragment extends Fragment{
         statsTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                statsBarChart.animateY(800, Easing.EasingOption.EaseInOutBack);
+                statsBarChart.animateY(500, Easing.EasingOption.EaseInOutBack);
                 if(tab.getPosition()==0){
-                    //TODO : Change range to daily
+                    currentViewRange = VIEWRANGE_DAILY;
+                    showChart(currentViewRange);
                 }
                 else if (tab.getPosition()==1){
-                    //TODO : Change range to Weekly
+                    currentViewRange = VIEWRANGE_WEEKLY;
+                    showChart(currentViewRange);
                 }
                 else if (tab.getPosition()==2){
-                    //TODO : Change range to MONTHLY
+                    currentViewRange = VIEWRANGE_MONTHLY;
+                    showChart(currentViewRange);
                 }
             }
 
@@ -185,11 +172,74 @@ public class StatsFragment extends Fragment{
     @Override
     public void onDetach() {super.onDetach();}
 
+    private void makeChart(ArrayList<BarEntry> yValues){
+        if(statsBarChart.getBarData()!=null) statsBarChart.getBarData().clearValues();
+        BarDataSet set1;
+        set1 = new BarDataSet(yValues, "Your Stats");
+        set1.setDrawIcons(false);
+        set1.setColors(getColors());
+        set1.setStackLabels(new String[]{"Focus Time", "Break Time"});
+
+        List<IBarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        BarData data = new BarData(dataSets);
+        data.setValueTextColor(Color.WHITE);
+
+        statsBarChart.setData(data);
+        }
+
+    private void showChart(int viewRange){
+        ArrayList<StatObject> statsListInRange = loadStats(viewRange);
+        ArrayList<BarEntry> yVals1 = addTimeValues(statsListInRange);
+        makeChart(yVals1);
+        statsBarChart.getBarData().notifyDataChanged();
+        statsBarChart.notifyDataSetChanged();
+
+    }
+
     private int[] getColors() {
 
-        int[] colors = {ColorTemplate.MATERIAL_COLORS[0],ColorTemplate.MATERIAL_COLORS[1]};
+        int color1 = ResourcesCompat.getColor(getResources(), R.color.timeCircleFinished,null);
+        int color2 = ResourcesCompat.getColor(getResources(), R.color.timeCircleUnfinished, null);
 
-        return colors;
+        return (new int[]{color1,color2});
+    }
+
+    private ArrayList<BarEntry> addTimeValues(List<StatObject> statlist) {
+        ArrayList<BarEntry> yValues = new ArrayList<>();
+        int listsize = statlist.size();
+        for (int i = 0; i < listsize; i++) {
+            StatObject stat = statlist.get(i);
+            float focustime = (float) stat.getFocusTime();
+            float breaktime = (float) stat.getBreakTime();
+
+            yValues.add(
+                    new BarEntry(TimeUnit.MILLISECONDS.toDays(stat.getTimeStamp()),
+                            new float[]{focustime, breaktime}));
+        }
+            return yValues;
+        }
+
+    private ArrayList<StatObject> loadStats(int viewRange){
+        ArrayList<StatObject> statsRequested = new ArrayList<>();
+        if(viewRange==VIEWRANGE_DAILY){
+            //load stats for last 7 days
+        }
+        else if(viewRange==VIEWRANGE_WEEKLY){
+            //Load stats for last 4 weeks
+        }
+        else if(viewRange==VIEWRANGE_MONTHLY){
+            //load stats for last 4(?) months
+        }
+        statsRequested.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("08-4-2018"),2,0));
+        statsRequested.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("09-4-2018"),25,17));
+        statsRequested.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("10-4-2018"),15,10));
+        statsRequested.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("11-4-2018"),20,15));
+        statsRequested.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("12-4-2018"),45,20));
+        statsRequested.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("13-4-2018"),35,10));
+        statsRequested.add(new StatObject(DayValueFormatter.convertDDMMYYYYDateStringToLongTimeMillis("14-4-2018"),25,5));
+        return statsRequested;
     }
 
 }
