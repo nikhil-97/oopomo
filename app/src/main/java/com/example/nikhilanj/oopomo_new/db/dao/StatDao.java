565f5c9f71ecf5177a5c4c2fde82a8e143f3a11a
Yeshwanth Vuppu <vyeshwanth123@gmail.com>
2018-04-16 22:56:43 +0530

    Add statObject and updateStatObject method in PomoTask class
    Signed-off-by: Yeshwanth Vuppu <vyeshwanth123@gmail.com>

--- app/src/main/java/com/example/nikhilanj/oopomo_new/db/dao/StatDao.java
+++ app/src/main/java/com/example/nikhilanj/oopomo_new/db/dao/StatDao.java
@@ -3,6 +3,7 @@ package com.example.nikhilanj.oopomo_new.db.dao;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import com.example.nikhilanj.oopomo_new.db.entity.StatObject;
 
@@ -19,4 +20,8 @@ public interface StatDao {
 
     @Insert
     long insert(StatObject stat);
+
+    @Update
+    void updateStatObject(StatObject statObject);
+
 }
