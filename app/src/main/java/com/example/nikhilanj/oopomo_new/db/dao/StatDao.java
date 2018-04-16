927592f4917b7e513bd5079a054a4f8abccb20fa
P V Nikhilanj <nikhilanj97@gmail.com>
2018-04-16 13:42:41 +0530

    minor feature updates

--- app/src/main/java/com/example/nikhilanj/oopomo_new/db/dao/StatDao.java
+++ app/src/main/java/com/example/nikhilanj/oopomo_new/db/dao/StatDao.java
@@ -4,16 +4,16 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
-import com.example.nikhilanj.oopomo_new.db.entity.Stat;
+import com.example.nikhilanj.oopomo_new.db.entity.StatObject;
 
 import java.util.List;
 
 @Dao
 public interface StatDao {
 
-    @Query("SELECT * FROM STAT WHERE stat_id = :id")
-    List<Stat> getStatById(long id);
+    @Query("SELECT * FROM STATOBJECT WHERE stat_id = :id")
+    List<StatObject> getStatById(long id);
 
     @Insert
-    long insert(Stat stat);
+    long insert(StatObject stat);
 }
--- app/src/main/java/com/example/nikhilanj/oopomo_new/utils/StatManager.java
+++ app/src/main/java/com/example/nikhilanj/oopomo_new/utils/StatManager.java
@@ -1,7 +1,7 @@
 package com.example.nikhilanj.oopomo_new.utils;
 
 import com.example.nikhilanj.oopomo_new.db.PomoDatabase;
-import com.example.nikhilanj.oopomo_new.db.entity.Stat;
+import com.example.nikhilanj.oopomo_new.db.entity.StatObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,7 +13,7 @@ public class StatManager {
         this.pomoDatabase = pomoDatabase;
     }
     public void insertStat(int focusTime, int breakTime, long timestamp) {
-        Stat stat = new Stat(focusTime, breakTime, timestamp);
+        StatObject stat = new StatObject(timestamp,focusTime, breakTime);
         pomoDatabase.statDao().insert(stat);
     }
 }
